55ac21d63cd89f7f697fe9a3f0c837b948d67640
digimori <hanaorikuu@gmail.com>
2023-09-17 10:52:08 +0000

    Committing MongoDB setup, as GGitpod needs restart to make ENV variables work.

--- server/index.js
+++ server/index.js
@@ -3,3 +3,20 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+//We're gonna need the process.env.REACT_APP_MONGODBURL, security my dude.
+
+const app = express();
+
+//Initial setup
+app.use(bodyParser.json({ limit: '30mb', extended: true}));  // This is because we're going to be using images and don't want to use too much data
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
+app.use(cors());  
+
+// MongoDB Setup
+const CONNECTION_URL = process.env.REACT_APP_MONGODBURL; // Connection URL for Database cluster - The credentials are stored in .env for security
+const PORT = process.env.PORT || 5000; // Port for deployment parameters
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) // Not mandatory, but helps prevent error warnings in console
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))  // This is like Try/Catch. If it connects successfully, the string will show else:
+    .catch((error) => console.log(error.message) )
+
+mongoose.set('strictQuery', true); // This is setting the database connection, the parameters are preventing unnecessary warnings.
