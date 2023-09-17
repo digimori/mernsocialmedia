c82fde90b764621d4e1000f26055f11fcf2b6390
digimori <hanaorikuu@gmail.com>
2023-09-17 11:26:52 +0000

    Create post logic implemented for backend. - Client Side not yet written.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,26 @@
+import PostMessage from "../models/postMessage.js";
+
 // req, res are the same across all route callbacks here.
 
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS');
+export const getPosts = async (req, res) => {
+    try {
+        // This needs to be an async as it takes time to fetch the data from MongoDB
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message})
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 
 const router = express.Router();
 
 // This is connected to the controllers/posts.js file, which contains the handlers for the post routes, it is passed as the second parameter here.
 router.get('/', getPosts);
+router.get('/', createPost);
 
 export default router;
